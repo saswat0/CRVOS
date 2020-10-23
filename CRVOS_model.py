81fba06bbfe69232e9fa3adfba957299a2f9e1bb
Saswat subhajyoti Mallick <saswatsubhajyotimallick@ZT-Mac-IN-saswat.mallick.bbrouter>
2020-10-23 21:12:41 +0530

    inital commit crvos model

---
+++ CRVOS_model.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import models
+
+
+DEVICE = torch.device("cuda")
+
+
+def softmax_aggregate(predicted_seg, object_ids):
+    bg_seg, _ = torch.stack([seg[:, 0, :, :] for seg in predicted_seg.values()], dim=1).min(dim=1)
+    bg_seg = torch.stack([1 - bg_seg, bg_seg], dim=1)
+    logits = {n: seg[:, 1:, :, :].clamp(1e-7, 1 - 1e-7) / seg[:, 0, :, :].clamp(1e-7, 1 - 1e-7)
+              for n, seg in [(-1, bg_seg)] + list(predicted_seg.items())}
+    logits_sum = torch.cat(list(logits.values()), dim=1).sum(dim=1, keepdim=True)
+    aggregated_lst = [logits[n] / logits_sum for n in [-1] + object_ids]
+    aggregated_inv_lst = [1 - elem for elem in aggregated_lst]
+    aggregated = torch.cat([elem for lst in zip(aggregated_inv_lst, aggregated_lst) for elem in lst], dim=-3)
+    final_seg_wrongids = aggregated[:, 1::2, :, :].argmax(dim=-3, keepdim=True)
+    assert final_seg_wrongids.dtype == torch.int64
+    final_seg = torch.zeros_like(final_seg_wrongids)
+    for idx, obj_idx in enumerate(object_ids):
+        final_seg[final_seg_wrongids == (idx + 1)] = obj_idx
+    return final_seg, {obj_idx: aggregated[:, 2 * (idx + 1):2 * (idx + 2), :, :] for idx, obj_idx in
+                       enumerate(object_ids)}
+
