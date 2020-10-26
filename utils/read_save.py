98b518eb7f79a3b323c2535b889bdbf31b9e5e5d
saswat <saswatsubhajyotimallick@gmail.com>
2020-10-26 09:45:09 +0530

    added custom read/save image

--- utils/read_save.py
+++ utils/read_save.py
@@ -1,3 +1,11 @@
+import threading
+import queue
+import time
+import os
+import png
+import numpy
+import math
+
 BASE_PALETTE_4BIT = [[0,   0,   0],
                      [236,  94, 102],
                      [249, 144,  87],
@@ -31,3 +39,13 @@ DAVIS_PALETTE_4BIT = [[0,   0,   0],
                       [191,   0, 128],
                       [64, 128, 128],
                       [191, 128, 128]]
+
+class ReadSaveImage(object):
+    def __init__(self):
+        super(ReadSaveImage, self).__init__()
+
+    def check_path(self, fullpath):
+        path, filename = os.path.split(fullpath)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
